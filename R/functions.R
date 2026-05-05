f683744fecc253af545baacd64d97be6f18721fd
Martin Lund <pyb7ih@rsyd.dk>
2026-05-05 16:22:46 +0200

    Trying R/functions.R

---
+++ R/functions.R
@@ -0,0 +1,17 @@
+#' read in one nurses data file
+#'
+#' @param file_path
+#' @param max_rows max rows to read
+#'
+#' @returns your taxes, fiscal year 1987, in the form of one data frame, tibble
+
+read <- function(file_path, max_rows = 100) {
+  data <- file_path |>
+    readr::read_csv(
+      show_col_types = FALSE,
+      name_repair = to_snake_case,
+      n_max = max_rows
+    )
+  return(data)
+}
+
